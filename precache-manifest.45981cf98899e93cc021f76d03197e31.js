026a9832aaacd082a053b146808a97f474ac9908
Shomee1015 <mrimmoaee@gmail.com>
2020-05-31 09:50:21 +0600

    Updates

---
+++ precache-manifest.45981cf98899e93cc021f76d03197e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a21b543b8fca05b756d377d48367836",
+    "url": "/Corona-Tracker/index.html"
+  },
+  {
+    "revision": "fe2db80ccd087836b3db",
+    "url": "/Corona-Tracker/static/css/main.5eac3b5e.chunk.css"
+  },
+  {
+    "revision": "08bc5b7f223d46b74443",
+    "url": "/Corona-Tracker/static/js/2.e8750492.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Tracker/static/js/2.e8750492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2db80ccd087836b3db",
+    "url": "/Corona-Tracker/static/js/main.42ac247f.chunk.js"
+  },
+  {
+    "revision": "995dcb91a1d109b30e84",
+    "url": "/Corona-Tracker/static/js/runtime-main.334d4e64.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Corona-Tracker/static/media/image.d7265326.png"
+  }
+]);
